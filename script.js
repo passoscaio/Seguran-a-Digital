e19765ecaa989b9e5ed624d20c3dd8a49855e4a9
passoscaio <caiofpassos09@gmail.com>
2026-08-25 10:55:02 -0300

    Senha de letras maiúsculas

--- script.js
+++ script.js
@@ -23,7 +23,40 @@ if (checkbox [3].checked) {
     ];
 }
 
-const letrasMaiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+geraSenha ();
+campoSenha.value = senha;
+
+
 const letrasMinusculas = "abcdefghijklmnopqrstuvwxyz";
 const numeros = "0123456789";
 const simbolos = "!?#+-@*%";
+
+
+function geraSenha(){
+    let senha = '' ”;
+    for (let i = 0; i < tamanhoSenha;i++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+}
+
+function diminuiTamanho(){
+    if (tamanhoSenha > 1){
+       // tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+       // tamanhoSenha = tamanhoSenha+1;
+       tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
