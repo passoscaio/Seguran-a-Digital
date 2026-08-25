48a276a0676f2cc04d49c9fca9d92f6a93df9c77
passoscaio <caiofpassos09@gmail.com>
2026-08-25 10:34:58 -0300

    Checkbox 0 a 3

--- script.js
+++ script.js
@@ -5,4 +5,20 @@ if (checkbox [0].checked) {
     math.floor (math.random() * letrasMaiusculas.length)
     ];
 }
-   
+   if (checkbox [1].checked) {
+ senha += letrasMinusculas [
+    math.floor (math.random() * letrasMinusculas.length)
+    ];
+}
+
+if (checkbox [2].checked) {
+ senha += numeros [
+    math.floor (math.random() * numeros.length)
+    ];
+}
+
+if (checkbox [3].checked) {
+ senha += simbolos [
+    math.floor (math.random() * simbolos.length)
+    ];
+}
